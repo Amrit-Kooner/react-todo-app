bbb5657747bfa1d7608b7d6cf75fa67f8febaeba
amrit <amritkooner47@gmail.com>
2024-08-01 18:50:48 +0100

    updated vite.config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "react-todo-app",
+  base: "/react-todo-app/",
   plugins: [react()]
 })
