ab2ee5a80280074000272a7b25453bbc6363b558
amrit <amritkooner47@gmail.com>
2024-08-01 18:44:42 +0100

    updated package.json to add deply and predeploy got gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/vite-deploy/"
+  base: "react-todo-app",
+  plugins: [react()]
 })
